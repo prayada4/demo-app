635c3ec3d97be09cc97be230dbb68a2f221af9bf
prayada4 <121497330+prayada4@users.noreply.github.com>
2023-01-10 19:43:45 +0530

    Update main.tf

--- main.tf
+++ main.tf
@@ -1,4 +1,14 @@
+data "tfe_outputs" "vpc" {
+  config = {
+    organization = "demo-tfe"
+    workspaces = {
+      name = "Network-Team"
+    }
+  }
+}
+
 module "ec2-instance" {
   source  = "app.terraform.io/demo-tfe/ec2-instance/aws"
-  version = "1.0.1"
+  version = "1.0.4"
+  mysubnet = data.tfe_outputs.vpc.outputs.vpc_subnets
 }
